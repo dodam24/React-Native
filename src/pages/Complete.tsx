80ea0a2aad608546e804ce3c9ad1da0fd932ea4a
dodam24 <dodam0724@gmail.com>
2024-04-03 22:11:19 +0900

    Update: 이미지 업로드 및 리사이즈

--- src/pages/Complete.tsx
+++ src/pages/Complete.tsx
@@ -1,15 +1,184 @@
-import React, {useState} from 'react';
-import {Pressable, Text, View} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {
+  Alert,
+  Dimensions,
+  Image,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {
+  NavigationProp,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
+import {LoggedInParamList} from '../../AppInner';
+import ImagePicker from 'react-native-image-crop-picker';
+import ImageResizer from 'react-native-image-resizer';
+import axios, {AxiosError} from 'axios';
+import Config from 'react-native-config';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
+import orderSlice from '../slices/order';
+import {useAppDispatch} from '../store';
 
 function Complete() {
-  const [count, setCount] = useState(1);
+  const dispatch = useAppDispatch();
+  const route = useRoute<RouteProp<LoggedInParamList>>();
+  const navigation = useNavigation<NavigationProp<LoggedInParamList>>();
+  const [image, setImage] =
+    useState<{uri: string; name: string; type: string}>();
+  const [preview, setPreview] = useState<{uri: string}>();
+  const accessToken = useSelector((state: RootState) => state.user.accessToken);
+
+  // { uri: '경로', filename: '파일 이름', type: '확장자' } 형태로 파일을 생성해서
+  // multipart/form-data를 통해서 파일 업로드
+
+  const onResponse = useCallback(async response => {
+    // console.log(response.width, response.height, response.exif);
+    setPreview({uri: `data:${response.mime};base64,${response.data}`}); // {uri: 주소} (이미지를 base64라는 텍스트로 변환)
+    const orientation = (response.exif as any)?.Orientation;
+    console.log('orientation', orientation);
+    // Image Resizing (이미지 용량 줄이기)
+    return ImageResizer.createResizedImage(
+      response.path, // 파일의 경로
+      600, // width
+      600, // height
+      response.mime.includes('jpeg') ? 'JPEG' : 'PNG', // format
+      100, // quality
+      0, // rotation
+    ).then(r => {
+      console.log(r.uri, r.name);
+
+      setImage({
+        uri: r.uri,
+        name: r.name,
+        type: response.mime,
+      });
+    });
+  }, []);
+
+  const onTakePhoto = useCallback(() => {
+    return ImagePicker.openCamera({
+      includeBase64: true,
+      includeExif: true, // 카메라 촬영 위치(방향)에 따른 숫자 표시
+    })
+      .then(onResponse)
+      .catch(console.log);
+  }, [onResponse]);
+
+  const onChangeFile = useCallback(() => {
+    return ImagePicker.openPicker({
+      includeExif: true,
+      includeBase64: true,
+      mediaType: 'photo',
+    })
+      .then(onResponse)
+      .catch(console.log);
+  }, [onResponse]);
+
+  const orderId = route.params?.orderId;
+  const onComplete = useCallback(async () => {
+    if (!image) {
+      Alert.alert('알림', '파일을 업로드해주세요.');
+      return;
+    }
+    if (!orderId) {
+      Alert.alert('알림', '유효하지 않은 주문입니다.');
+      return;
+    }
+    const formData = new FormData(); // FormData를 통해서 이미지 업로드
+    formData.append('orderId', orderId);
+    formData.append('image', {
+      name: image.name,
+      type: image.type || 'image/jpeg',
+      uri:
+        Platform.OS === 'android'
+          ? image.uri
+          : image.uri.replace('file://', ''),
+    });
+    console.log(formData.getParts());
+    try {
+      await axios.post(`${Config.API_URL}/complete`, formData, {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      });
+      Alert.alert('알림', '완료처리 되었습니다.');
+      navigation.goBack();
+      navigation.navigate('Settings');
+      dispatch(orderSlice.actions.rejectOrder(orderId));
+    } catch (error) {
+      const errorResponse = (error as AxiosError).response;
+      if (errorResponse) {
+        Alert.alert('알림', errorResponse.data.message);
+      }
+    }
+  }, [dispatch, navigation, image, orderId, accessToken]);
+
   return (
     <View>
-      <Pressable onPress={() => setCount(p => p + 1)}>
-        <Text>{count}</Text>
-      </Pressable>
+      <View style={styles.orderId}>
+        <Text>주문번호: {orderId}</Text>
+      </View>
+      <View style={styles.preview}>
+        {preview && <Image style={styles.previewImage} source={preview} />}
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Pressable style={styles.button} onPress={onTakePhoto}>
+          <Text style={styles.buttonText}>이미지 촬영</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={onChangeFile}>
+          <Text style={styles.buttonText}>이미지 선택</Text>
+        </Pressable>
+        <Pressable
+          style={
+            image
+              ? styles.button
+              : StyleSheet.compose(styles.button, styles.buttonDisabled)
+          }
+          onPress={onComplete}>
+          <Text style={styles.buttonText}>완료</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  orderId: {
+    padding: 20,
+  },
+  preview: {
+    marginHorizontal: 10,
+    width: Dimensions.get('window').width - 20,
+    height: Dimensions.get('window').height / 3,
+    backgroundColor: '#D2D2D2',
+    marginBottom: 10,
+  },
+  previewImage: {
+    height: Dimensions.get('window').height / 3,
+    resizeMode: 'contain',
+  },
+  buttonWrapper: {flexDirection: 'row', justifyContent: 'center'},
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    width: 120,
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+    borderRadius: 5,
+    margin: 5,
+  },
+  buttonText: {
+    color: 'black',
+  },
+  buttonDisabled: {
+    backgroundColor: 'gray',
+  },
+});
 
 export default Complete;
